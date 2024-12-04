f34277dfa3eb15000762bff0a864861d6214d4a4
Noel Kujur <lovenoelkujur@gmail.com>
2024-12-04 20:43:22 +0530

    Cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const router = require("./Routes/task");
+const cors = require("cors");
 const databaseConnection = require("./Utils/database");
 const scheduleTask = require("./Utils/scheduler");
 
@@ -15,6 +16,15 @@ app.use(express.json());
 
 // Main Route
 app.use("/api/v1", router);
+
+// Cors
+app.use(
+    cors({
+        origin : "http://localhost:5173/",
+        methods : ["GET", "POST", "PUT", "DELETE"],
+        credentials : true,
+    })
+);
 
 // DB Connection
 databaseConnection();
