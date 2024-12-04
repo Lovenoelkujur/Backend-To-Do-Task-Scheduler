0358ebffabc12d3477e69049dec8db34e7d48273
Noel Kujur <lovenoelkujur@gmail.com>
2024-12-04 22:30:09 +0530

    Cors update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api/v1", router);
 // Cors
 app.use(
     cors({
-        origin : "http://localhost:5173",
+        origin : "https://frontend-to-do-task-scheduler.vercel.app",
         methods : ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
