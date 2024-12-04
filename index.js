2c83d6e9870d8892b3d3821a8bcfe1a5b85f23e2
Noel Kujur <lovenoelkujur@gmail.com>
2024-12-04 21:57:19 +0530

    cors fixed -2

--- index.js
+++ index.js
@@ -23,8 +23,11 @@ app.use(
         origin : "http://localhost:5173",
         methods : ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 );
+
+app.options("*", cors()); // Handle preflight requests
 
 
 // DB Connection
