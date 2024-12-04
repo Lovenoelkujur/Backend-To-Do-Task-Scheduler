4d60452da111e848f3ce1f67067b00bb1b7cad53
Noel Kujur <lovenoelkujur@gmail.com>
2024-12-04 20:53:22 +0530

    cors error fix-1

--- index.js
+++ index.js
@@ -20,11 +20,12 @@ app.use("/api/v1", router);
 // Cors
 app.use(
     cors({
-        origin : "http://localhost:5173/",
+        origin : "http://localhost:5173",
         methods : ["GET", "POST", "PUT", "DELETE"],
         credentials : true,
     })
 );
+
 
 // DB Connection
 databaseConnection();
