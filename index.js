c9778e24047eac6eff121cf24eeea517fff4c6ef
Noel Kujur <lovenoelkujur@gmail.com>
2024-12-04 21:45:57 +0530

    cors updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
     cors({
         origin : "http://localhost:5173",
         methods : ["GET", "POST", "PUT", "DELETE"],
-        credentials : true,
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
